b8a6465eb8464c0ab606fa88a0214e702c146990
Ricardo Muñoz <MunozRic@protrans.com>
2023-08-19 00:21:13 -0600

    Added npm

--- backend/src/Services/CatalogService/Catalog.API/Program.cs
+++ backend/src/Services/CatalogService/Catalog.API/Program.cs
@@ -5,6 +5,7 @@ using Catalog.Infrastructure;
 using Catalog.Infrastructure.Data;
 using Common.Api.Middlewares;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,10 +18,16 @@ builder.Services.AddConfigDBServices(builder.Configuration, builder.Environment.
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddRateLimitServices(builder.Configuration);
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
 
 
 var app = builder.Build();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+}
 // Configure the HTTP request pipeline.
 app.UseDefaultFiles();
 app.UseStaticFiles();
