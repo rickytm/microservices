12fdfa3cf04b08dba7842ce6e73643ff919549fb
Ricardo Muñoz <MunozRic@protrans.com>
2023-08-19 01:11:03 -0600

    Added more services

--- backend/src/Services/Localization/Localization.API/Program.cs
+++ backend/src/Services/Localization/Localization.API/Program.cs
@@ -5,12 +5,14 @@ using Localization.Application;
 using Localization.Infrastructure;
 using Localization.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //builder.Logging.ClearProviders();
 //builder.Logging.AddConsole().AddConsoleFormatter<CustomTimePrefixingFormatter, CustomWrappingConsoleFormatterOptions>();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
 
 builder.Services.AddLocalizationApiServices();
 builder.Services.AddConfigDBServices(builder.Configuration, builder.Environment.IsDevelopment());
@@ -21,6 +23,12 @@ builder.Services.AddRateLimitServices(builder.Configuration);
 
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Localizacion.API v1"));
+}
 
 // Configure the HTTP request pipeline.
 app.UseDefaultFiles();
