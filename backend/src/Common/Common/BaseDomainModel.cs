4bd6cf1a798e5c48986ba15a6913de55db792e73
Ricardo Muñoz <MunozRic@protrans.com>
2023-08-20 21:18:46 -0600

    A

--- backend/src/Common/Common/BaseDomainModel.cs
+++ backend/src/Common/Common/BaseDomainModel.cs
@@ -7,6 +7,6 @@ namespace Common
         public DateTime? CreatedDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? LastModifiedDate { get; set; }
-        public string? LastModifiedBy { get; set; }
+        public string LastModifiedBy { get; set; }
     }
 }
